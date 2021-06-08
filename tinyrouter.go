123bfcc96efc61fb029f659168099b2062ed1e97
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-06-08 22:10:27 +0900

    メソッドの雛形作成

--- tinyrouter.go
+++ tinyrouter.go
@@ -17,3 +17,30 @@ type Router interface {
 
 	Use(func(http.ResponseWriter, *http.Request) func(http.ResponseWriter, *http.Request))
 }
+
+// TinyRouter implements http.Handler interface.
+type TinyRouter struct{}
+
+func NewRouter() *TinyRouter {
+	return &TinyRouter{}
+}
+
+func (t *TinyRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+
+func (t *TinyRouter) Head(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Get(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Post(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Put(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Patch(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Delete(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Options(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Connect(path string, f func(http.ResponseWriter, *http.Request)) {}
+
+func (t *TinyRouter) Trace(path string, f func(http.ResponseWriter, *http.Request)) {}
