f03043a3413e2aca1fc69ceaeb55b1a11b05aeca
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2021-06-10 23:40:57 +0900

    makePattern関数の実装

--- route.go
+++ route.go
@@ -1,13 +1,60 @@
 package tinyrouter
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
+)
+
+var (
+	UnbalancedBracesError = errors.New("tinyrouter: unbalanced braces")
 )
 
 type Route struct {
+	ParamName  []string
 	Method     string
 	Path       string
+	Pattern    regexp.Regexp
 	HandleFunc func(http.ResponseWriter, *http.Request)
+}
+
+func makePattern(path string, indexList []int) string {
+	var builder strings.Builder
+	if len(indexList) == 0 {
+		builder.WriteString(path)
+	} else {
+		var start int = 0
+		var next int
+		for i := 0; i < len(indexList); i += 2 {
+			next = indexList[i]
+			builder.WriteString(path[start:next])
+			builder.WriteString("([^/]+)")
+			start = indexList[i+1] + 1
+		}
+	}
+	return fmt.Sprintf("^%s$", builder.String())
+}
+
+func bracesIndex(path string) ([]int, error) {
+	indexList := make([]int, 0)
+	checkBraces := 0
+	for i := 0; i < len(path); i++ {
+		switch path[i] {
+		case '{':
+			if checkBraces++; checkBraces != 1 {
+				return nil, UnbalancedBracesError
+			}
+			indexList = append(indexList, i)
+		case '}':
+			if checkBraces--; checkBraces != 0 {
+				return nil, UnbalancedBracesError
+			}
+			indexList = append(indexList, i)
+		}
+	}
+	return indexList, nil
 }
 
 func NewRoute(method string, path string, f func(http.ResponseWriter, *http.Request)) *Route {
--- route_test.go
+++ route_test.go
@@ -26,7 +26,57 @@ func TestMakePattern(t *testing.T) {
 	}
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
-			actual := makePattern(tc.path)
+			indexList, _ := bracesIndex(tc.path)
+			actual := makePattern(tc.path, indexList)
+			assert.Equal(t, tc.expect, actual)
+		})
+	}
+}
+
+func TestBracesIndex(t *testing.T) {
+	cases := map[string]struct {
+		path        string
+		expect      []int
+		expectError error
+	}{
+		"path without braces": {
+			path:        "/todo",
+			expect:      []int{},
+			expectError: nil,
+		},
+		"path with braces": {
+			path:        "/todo/{id}",
+			expect:      []int{6, 9},
+			expectError: nil,
+		},
+		"path with multiple param": {
+			path:        "/todo/{id}/{field}",
+			expect:      []int{6, 9, 11, 17},
+			expectError: nil,
+		},
+		"path with unbalanced braces(braces isn't close)": {
+			path:        "/todo/{i{d}",
+			expect:      nil,
+			expectError: UnbalancedBracesError,
+		},
+		"path with unbalanced braces(braces starts with '}')": {
+			path:        "/todo/}id}",
+			expect:      nil,
+			expectError: UnbalancedBracesError,
+		},
+		"path with unbalanced braces('}' is continuous)": {
+			path:        "/todo/{id}}",
+			expect:      nil,
+			expectError: UnbalancedBracesError,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			actual, err := bracesIndex(tc.path)
+			if tc.expectError != nil {
+				assert.Equal(t, tc.expectError, err)
+			}
 			assert.Equal(t, tc.expect, actual)
 		})
 	}
